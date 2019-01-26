893cf6b27307d7b53c591dc1639fa12b7e621455
SiFlAMeSE <flame_uz@hotmail.com>
2019-01-26 13:35:40 +0700

    update v.new

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -2,28 +2,45 @@ const express = require('express');
 const app = express();
 const UserRouter = express.Router();
 
+
 const UserModel = require('../model/UserModel');
 
-UserRouter.route('/add').post(function(req, res){
+
+UserRouter.route('/add').post(function (req, res) {
     const user = new UserModel(req.body);
     user.save()
-    .then(user =>{
-        res.json('Server added successfully');
-    })
-    .catch(err => {
-        res.status(400).send("unable to save to database");
-    });
+        .then(user => {
+            res.json('Server added successfully');
+        })
+        .catch(err => {
+            res.status(400).send("unable to save to database");
+        });
 });
 
-UserRouter.route('/user_list').get(function(req, res) {
-    UserModel.find(function(err, user) {
-        if(err){
+UserRouter.route('/user_list').get(function (req, res) {
+    UserModel.find(function (err, user) {
+        if (err) {
             console.log(err);
         }
         else {
             res.json(user);
         }
     });
+});
+
+UserRouter.route('/login').post(function (req, res) {
+    var user = req.body.User_g
+    var pass = req.body.Password
+    UserModel.findOne({ User_g: user, Password: pass }, (err, result) => {
+        if (err) { /* handle err */ }
+
+        if (result) {
+            res.send({ status: "Success", data: result });
+        } else {
+            res.send({ status: "Fail" });
+        }
+    })
+
 });
 
 module.exports = UserRouter;
