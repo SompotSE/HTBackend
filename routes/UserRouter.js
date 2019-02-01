f9e292f2a3a7f3a56cf923fab5ac62a0081c96d0
SiFlAMeSE <flame_uz@hotmail.com>
2019-02-01 23:16:09 +0700

    update userrouter

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -31,7 +31,7 @@ UserRouter.route('/user_list').get(function (req, res) {
 
 UserRouter.route('/login').post(function (req, res) {
     var user = req.body.User_g
-    var pass = req.body.Password
+    var pass = req.body.Password 
     UserModel.findOne({ User_g: user, Password: pass }, (err, result) => {
         if (err) { /* handle err */ }
 
