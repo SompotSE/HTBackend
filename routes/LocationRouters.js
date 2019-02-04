39d97b39d7d3c86c133752a23da47c548acd0416
SiFlAMeSE <flame_uz@hotmail.com>
2019-02-04 22:42:49 +0700

    insert function delete location

--- routes/LocationRouters.js
+++ routes/LocationRouters.js
@@ -4,24 +4,35 @@ const LocationRouter = express.Router();
 
 const LocationModel = require('../model/LocationModel');
 
-LocationRouter.route('/add').post(function(req, res){
+LocationRouter.route('/add').post(function (req, res) {
     const loca = new LocationModel(req.body);
     loca.save()
-    .then(loca =>{
-        res.json('Server added successfully');
-    })
-    .catch(err => {
-        res.status(400).send("unable to save to database");
+        .then(loca => {
+            res.json('Server added successfully');
+        })
+        .catch(err => {
+            res.status(400).send("unable to save to database");
+        });
+});
+
+LocationRouter.route('/location_list').get(function (req, res) {
+    LocationModel.find(function (err, loca) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.json(loca);
+        }
     });
 });
 
-LocationRouter.route('/location_list').get(function(req, res){
-    LocationModel.find(function(err, loca){
-        if(err){
-            console.log(err);
-        }
-        else{
-            res.json(loca);
+LocationRouter.route('/Removelocation').post(function (req, res, _id) {
+    LocationModel.findOneAndDelete(_id, function (err, loca) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.json("Location has been Deleted")
+            console.log('send it')
         }
     });
 });
